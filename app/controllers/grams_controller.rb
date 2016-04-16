10b664778ef1e0b8d20c89e7093d2943d3b1cca6
Bazza <barryonglb@gmail.com>
2016-04-16 08:37:42 +0000

    saturday eod

---
+++ app/controllers/grams_controller.rb
@@ -0,0 +1,40 @@
+class GramsController < ApplicationController
+  def index
+    @grams =Gram.all
+  end
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+    @gram = Gram.new(model_params)
+    if @gram.save
+        redirect_to @gram
+    else
+      render :new
+    end
+
+  end
+
+  def show
+    @gram = Gram.find(params[:id])
+  end
+
+  def destroy
+    @gram = Gram.find(params[:id])
+    @gram.destroy
+
+    redirect_to grams_path
+  end
+
+  private
+
+  def model_params
+    params.require(:gram).permit(:title, :description, :location_code)
+    #"gram"=>{"title"=>"xxx", "description"=>"xxx", "location_code"=>"xxx"}
+  end
+
+
+
+end
